3309a0433891dcda6abca7ffd89a3a296c1a1106
Artem Krotov <akrotov.direct@gmail.com>
2021-02-28 02:17:11 +0300

    Init

---
+++ src/main/java/cmind/redyoungteam/telegramservice/TelegramserviceApplication.java
@@ -0,0 +1,13 @@
+package cmind.redyoungteam.telegramservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cmind/redyoungteam/telegramservice/TelegramserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package cmind.redyoungteam.telegramservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
